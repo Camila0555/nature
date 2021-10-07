a6aac31e88bcf706a432f7adf5d1ec3ae89f9bae
Camila0555 <88430336+Camila0555@users.noreply.github.com>
2021-10-07 22:11:00 +0900

    Update JSanime.js

--- JSanime.js
+++ JSanime.js
@@ -5,7 +5,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+mybutton = document.getElementById("myBtn");
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+if(document.body.scrollTop > 20 ||
+   document.documentElement.scrollTop > 20){
+  mybutton.style.display =" block";
+}else{ 
+  mybutton.style.display = "none";
+}
+}
 
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
